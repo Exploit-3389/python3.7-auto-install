79616ca11c3c9daebc8a35f78777a690e76d5823
Exploit-3389 <61142764+Exploit-3389@users.noreply.github.com>
2020-02-29 12:52:57 +0800

    Create py3.7-install.sh

---
+++ py3.7-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
+wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz &&
+mkdir -p /usr/local/python3/ &&
+tar -xvf Python-3.7.6.tgz &&
+cd Python-3.7.6 &&
+./configure --prefix=/usr/local/python3/ &&
+make &&
+make install &&
+ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
+ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3 &&
+pip3 install --upgrade pip &&
+echo "python3.7 installed successfullly!"
