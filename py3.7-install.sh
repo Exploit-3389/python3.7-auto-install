9cfac1ead6713cc9a61cd47f4536a84fbef8722c
Exploit-3389 <61142764+Exploit-3389@users.noreply.github.com>
2020-03-06 13:06:07 +0800

    Update py3.7-install.sh

--- py3.7-install.sh
+++ py3.7-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-yum -y install gcc make zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
+yum -y install gcc make libffi-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel &&
 wget https://www.python.org/ftp/python/3.7.6/Python-3.7.6.tgz &&
 mkdir -p /usr/local/python3/ &&
 tar -xvf Python-3.7.6.tgz &&
 cd Python-3.7.6 &&
-./configure --prefix=/usr/local/python3/ &&
+./configure --prefix=/usr/local/python3/ --enable-optimizations  &&
 make &&
 make install &&
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
